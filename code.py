3a492476f12ee6be44ce66ad168c523024b06f24
Pranav0525 <agarwalpranav227@gmail.com>
2025-09-28 22:21:26 +0530

    added pipes

--- code.py
+++ code.py
@@ -3,11 +3,6 @@ import sys
 
 pygame.init()
 
-bird_x = 50
-bird_y = 300
-bird_width = 30
-bird_height = 30
-bird_color = (255, 255, 0)
 
 gravity = 0.5
 bird_velocity = 0
@@ -17,9 +12,19 @@ WIDTH, HEIGHT = 400, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("FLAPPY BIRD")
 
-
 clock = pygame.time.Clock()
 FPS = 60
+
+bird_x = 50
+bird_y = 300
+bird_width = 30
+bird_height = 30
+bird_color = (255, 255, 0)
+pipe_width = 60
+pip_gap = 150
+pipe_color = (0, 255, 0)
+pipe_x = WIDTH
+pipe_height = 200
 
 running = True
 while running:
@@ -29,6 +34,14 @@ while running:
     bird_y += bird_velocity
 
     pygame.draw.rect(screen, bird_color, (bird_x, bird_y, bird_width, bird_height))
+    pipe_x -= 3
+
+    pygame.draw.rect(screen, pipe_color, (pipe_x, 0, pipe_width, pipe_height))
+
+    pygame.draw.rect(screen, pipe_color, (pipe_x, pipe_height + pip_gap, pipe_width, HEIGHT))
+
+    if pipe_x < -pipe_width:
+        pipe_x = WIDTH
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,7 +49,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_velocity = jump_strength
-        
+
     pygame.display.update()
 
 pygame.quit()
